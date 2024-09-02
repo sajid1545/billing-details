a52f58b47e329967786525deca0cc0b4fdd8f0ab
Sajjad Abdullah <clashking1545@gmail.com>
2024-09-02 12:45:40 +0600

    updated

--- src/BillingDetails.jsx
+++ src/BillingDetails.jsx
@@ -16,7 +16,7 @@ const planData = {
 			maxDailyDrawdown: 5,
 			maxDrawdown: 10,
 			tradingPeriod: "30 days",
-			profitTarget: 8000,
+			profitTarget: 8,
 			minTradingDays: 10,
 			newsTrading: true,
 			weekendHolding: false,
@@ -29,7 +29,7 @@ const planData = {
 			maxDailyDrawdown: 5,
 			maxDrawdown: 10,
 			tradingPeriod: "60 days",
-			profitTarget: 5000,
+			profitTarget: 5,
 			minTradingDays: 10,
 			newsTrading: true,
 			weekendHolding: false,
@@ -55,7 +55,7 @@ const planData = {
 };
 
 const formData = {
-	email: "sajid@gmail.com",
+	email: "sajid2@gmail.com",
 	first: "Sajid",
 	last: "Abd",
 	country: "BD",
@@ -67,50 +67,45 @@ const formData = {
 
 const BillingDetails = () => {
 	const createUser = useMutation({
-		mutationFn: (data) =>
-			apiRequestHandler("/users/normal-register", "POST", data),
+		//   user registration taking place here
+		mutationFn: (data) => apiRequestHandler("/users/normal-register", "POST", data),
 
 		onSuccess: async (data) => {
-			// const paymentStatus = true;
+			// User registration response
+			const userResponse = data;
+
+			// Check if user registration was successful
+			if (!userResponse) {
+				toast.error("User registration failed.");
+				return;
+			}
+
+			// TODO: Calculate `subTotal`, `discountPrice`, and `totalPrice` based on `planData` and any applied discounts
+			const orderData = {
+				orderItems: [planData],
+				paymentMethod: "Stripe", // TODO: Update with selected payment method, will be dynamic
+				buyerDetails: {
+					email: userResponse.email,
+					first: userResponse.first,
+					last: userResponse.last,
+					country: userResponse.country,
+					addr: userResponse.addr,
+					city: userResponse.city,
+					zipCode: userResponse.zipCode,
+					phone: userResponse.phone,
+					userId: userResponse._id,
+					password: userResponse.password, // Ensure secure handling of passwords
+				},
+				subtotal: 0, // TODO: Update with calculated subtotal
+				discountPrice: 0, // TODO: Update with calculated discount price
+				totalPrice: 0, // TODO: Update with calculated total price
+				// couponClaimed: couponData._id, // TODO: Uncomment and use if applicable
+			};
 
 			try {
-				// User registration response
-				const userResponse = data;
-
-				// Check if user registration was successful
-				if (!userResponse) {
-					toast.error("User registration failed.");
-					return;
-				}
-
-				// Prepare order data with user and plan details
-				const orderData = {
-					orderItems: [planData],
-					paymentMethod: "Stripe",
-					buyerDetails: {
-						email: userResponse.email,
-						first: userResponse.first,
-						last: userResponse.last,
-						country: userResponse.country,
-						addr: userResponse.addr,
-						city: userResponse.city,
-						zipCode: userResponse.zipCode,
-						phone: userResponse.phone,
-						userId: userResponse._id,
-						password: userResponse.password,
-					},
-					subtotal: 0,
-					discount_price: 0,
-					total_price: 0,
-					// couponClaimed: couponData._id,
-				};
-
 				// Create the order for the user
-				const orderResponse = await apiRequestHandler(
-					"/orders/create-order",
-					"POST",
-					orderData,
-				);
+				const orderResponse = await apiRequestHandler("/orders/create-order", "POST", orderData);
+
 				// Check if order creation was successful
 				if (!orderResponse) {
 					toast.error("Failed to create order.");
@@ -118,13 +113,9 @@ const BillingDetails = () => {
 				}
 
 				// Update the user with the new order ID
-				const updateUser = await apiRequestHandler(
-					`/users/${userResponse._id}`,
-					"PUT",
-					{
-						orders: [orderResponse._id],
-					},
-				);
+				const updateUser = await apiRequestHandler(`/users/${userResponse._id}`, "PUT", {
+					orders: [orderResponse._id],
+				});
 
 				// Check if user update with order ID was successful
 				if (!updateUser) {
@@ -132,89 +123,125 @@ const BillingDetails = () => {
 					return;
 				}
 
-				const updateUserPurchaseProducts = await apiRequestHandler(
-					`/users/${userResponse._id}/purchased-products`,
-					"PUT",
-					{
-						productId: orderResponse.orderId,
-						product: planData,
-					},
-				);
+				// TODO :: Simulate payment status (this should be handled by your payment processor)
+				const paymentIsDone = true;
 
-				if (!updateUserPurchaseProducts) {
-					toast.error("Failed to create order.");
-					return;
+				if (paymentIsDone) {
+					// Update order status and payment status if payment was successful
+					const orderStatusUpdate = await apiRequestHandler(`/orders/${orderResponse._id}`, "PUT", {
+						orderStatus: "Accepted",
+						paymentStatus: "Paid",
+					});
+
+					// Check if order status update was successful
+					if (
+						orderStatusUpdate.paymentStatus !== "Paid" ||
+						orderStatusUpdate.orderStatus !== "Accepted"
+					) {
+						toast.error("Order or payment status update failed.");
+						return;
+					}
+
+					// Update user purchase products after order is placed
+					const updateUserPurchaseProducts = await apiRequestHandler(
+						`/users/${userResponse._id}/purchased-products`,
+						"PUT",
+						{
+							productId: orderResponse.orderId,
+							product: planData, // TODO: Update with actual challenge data
+						}
+					);
+
+					// Check if updating user purchase products was successful
+					if (!updateUserPurchaseProducts) {
+						toast.error("Failed to update user purchased products.");
+						return;
+					}
+
+					// Create MT5 account
+
+					// TODO: Create MT5 account through API integration
+					const mt5CreatedThroughApi = true; // Simulated response, replace with actual API call
+
+					// Check if MT5 account creation was successful
+					if (!mt5CreatedThroughApi) {
+						await apiRequestHandler(`/orders/${orderResponse._id}`, "PUT", {
+							orderStatus: "Processing", // Set to Processing if MT5 account creation fails
+						});
+						toast.error("Failed to create MT5 account.");
+						return;
+					}
+
+					if (mt5CreatedThroughApi) {
+						//! Section start: MT5 account creation through API
+						// TODO: This section will be updated after actual api call
+						const productId =
+							updateUserPurchaseProducts.data.purchasedProducts[orderResponse.orderId].productId;
+
+						const product =
+							updateUserPurchaseProducts.data.purchasedProducts[orderResponse.orderId].product;
+
+						// Determine challenge stage based on product type
+						let challengeStage = product?.challengeType === "funded" ? "funded" : "phase1";
+						// Prepare challenge stage data for injecting MT5 account in user's collection
+						const challengeStageData = {
+							...product,
+							challengeStages: {
+								...product.challengeStages,
+								phase1: challengeStage === "funded" ? null : product.challengeStages.phase1,
+								phase2: challengeStage === "funded" ? null : product.challengeStages.phase2,
+								funded: challengeStage === "phase1" ? null : product.challengeStages.funded,
+							},
+						};
+
+						// Simulated MT5 account data
+						const mt5Data = {
+							account: 999232, // Example account number
+							password: "43252@!", // Example password (ensure secure handling)
+							productId: productId,
+							challengeStage: challengeStage,
+							challengeStageData: challengeStageData,
+						};
+
+						//! Section ends: MT5 account creation through API
+
+						// Inject MT5 account data in user's collection
+						const updateMT5Account = await apiRequestHandler(`/users/${userResponse._id}`, "PUT", {
+							mt5Accounts: [mt5Data],
+						});
+
+						// Check if MT5 account update was successful
+						if (!updateMT5Account) {
+							toast.error("Failed to update user MT5 account.");
+							return;
+						}
+
+						// Update the order status to Delivered
+						const updateOrderStatus = await apiRequestHandler(
+							`/orders/${orderResponse._id}`,
+							"PUT",
+							{
+								orderStatus: "Delivered",
+							}
+						);
+
+						// Check if order status update to Delivered was successful
+						if (!updateOrderStatus) {
+							toast.error("Failed to update order status to Delivered.");
+							return;
+						}
+
+						// Update the user's role to trader
+						await apiRequestHandler(`/users/${userResponse._id}`, "PUT", {
+							role: "trader",
+						});
+
+						// Notify user of successful payment processing
+						toast.success("Payment processed successfully!");
+					}
 				}
-
-				const productId =
-					updateUserPurchaseProducts.data.purchasedProducts[
-						orderResponse.orderId
-					].productId;
-
-                const product = updateUserPurchaseProducts.data.purchasedProducts[
-                    orderResponse.orderId
-                ].product
-
-                console.log(product);
-
-                let challengeStage;
-
-                if (product?.challengeType === "funded") {
-                    challengeStage = "funded";
-                } else {
-                    challengeStage = "phase1";
-                }
-
-                const challengeStageData = {
-                    ...product,
-                    ...product,
-                    challengeStages: {
-                      ...product.challengeStages,
-                      phase1: challengeStage === "funded" ? null : product.challengeStages.phase1,
-                      phase2: challengeStage === "funded" || challengeStage === "phase1" && null,
-                      funded: challengeStage === "phase1" ? null : product.challengeStages.funded
-                    }
-                }
-
-				const mt5Data = {
-					account: 999232,
-					password: "43252@!",
-					productId: productId,
-					challengeStage: challengeStage,
-
-                    challengeStageData: challengeStageData
-						
-				};
-
-				const updateMT5Account = await apiRequestHandler(
-					`/users/${userResponse._id}`,
-					"PUT",
-					{
-						mt5Accounts: [mt5Data],
-					},
-				);
-
-				if (!updateMT5Account) {
-					toast.error("Failed to update user mt5Account.");
-					return;
-				}
-
-				// Update the user with the new order ID
-				// const updateUser = await apiRequestHandler(
-				// 	`/users/${userResponse._id}`,
-				// 	"PUT",
-				// 	{
-				// 		orders: [orderResponse._id],
-				// 	},
-				// );
-
-				// // Check if user update with order ID was successful
-				// if (!updateUser) {
-				// 	toast.error("Failed to update user with new order.");
-				// 	return;
-				// }
 			} catch (error) {
-				// Log the error and notify user
+				// Log the error and notify the user
 				console.error("🚀 ~ onSuccess error:", error);
 				toast.error(`An error occurred during the process: ${error.message}`);
 			}
@@ -236,8 +263,7 @@ const BillingDetails = () => {
 				<button
 					onClick={(e) => onSubmit(e)}
 					type="submit"
-					className="px-10 py-2 bg-blue-600 hover:bg-blue-500 duration-500 rounded-md w-2/4 text-white font-bold"
-				>
+					className="px-10 py-2 bg-blue-600 hover:bg-blue-500 duration-500 rounded-md w-2/4 text-white font-bold">
 					{createUser.isPending ? "Processing..." : "Proceed"}
 				</button>
 			</div>
