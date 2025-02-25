ca790327c8664e551089545916abf05250c426f9
Sajjad Abdullah <clashking1545@gmail.com>
2025-02-25 16:20:11 +0600

    updated

--- src/utils/apiRequestHandler.js
+++ src/utils/apiRequestHandler.js
@@ -1,30 +1,29 @@
 import axios from "axios";
 
 const apiRequestHandler = async (routePath, method, data = null, token = null) => {
-	try {
-		const axiosPublic = axios.create({
-			baseURL: "http://localhost:5000/api/foxx-funded/v1",
-			// baseURL: `${import.meta.env.VITE_BASE_URL}/api/summit/v1`,
-		});
+    try {
+        const axiosPublic = axios.create({
+            baseURL: `${import.meta.env.VITE_BASE_URL}/api/foxx-funded/v1`,
+        });
 
-		const url = `${axiosPublic.defaults.baseURL}${routePath}`;
+        const url = `${axiosPublic.defaults.baseURL}${routePath}`;
 
-		const config = {
-			method,
-			url,
-			headers: {
-				"Content-Type": "application/json",
-				...(token && { Authorization: `Bearer ${token}` }),
-			},
-			...(data && { data: JSON.stringify(data) }),
-		};
+        const config = {
+            method,
+            url,
+            headers: {
+                "Content-Type": "application/json",
+                ...(token && { Authorization: `Bearer ${token}` }),
+            },
+            ...(data && { data: JSON.stringify(data) }),
+        };
 
-		const response = await axiosPublic(config);
-		return response.data;
-	} catch (error) {
-		console.error("API call error:", error);
-		throw error;
-	}
+        const response = await axiosPublic(config);
+        return response.data;
+    } catch (error) {
+        console.error("API call error:", error);
+        throw error;
+    }
 };
 
 export default apiRequestHandler;
